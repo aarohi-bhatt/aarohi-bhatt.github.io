49f95a7089d631fbc8d4eb99a619e81ba907f1eb
Aarohi Bhatt <bhattaarohik@gmail.com>
2026-09-10 00:05:25 -0400

    Update education.ts

--- src/data/education.ts
+++ src/data/education.ts
@@ -13,9 +13,8 @@ export const educationData: Education[] = [
     year: "2026-2028",
     institution: "Columbia University, Fu Foundation School of Engineering and Applied Science",
     degree: "MSCS Bridge @ Columbia University",
-    thesis: "Thesis-oriented preparation in computer science and machine learning",
     coursework:
-      "Summer 2026: COMS W1004 (Intro to Programming in Java), COMS W3134 (Data Structures in Java), COMS W3203 (Discrete Mathematics), MATH UN1201 (Calculus III). Fall 2026: COMS W3157 (Advanced Programming), MATH UN2015 (Linear Algebra & Probability)",
+      "Summer 2026: COMS W1004 (Intro to Programming in Java), COMS W3134 (Data Structures in Java), COMS W3203 (Discrete Mathematics), MATH UN1201 (Calculus III). Fall 2026: COMS W3157 (Advanced Programming), MATH UN2015 (Linear Algebra & Probability); waitlisted: COMS W4705 (Natural Language Processing), COMS W4771 (Machine Learning)",
   },
   {
     year: "2017-2023",
